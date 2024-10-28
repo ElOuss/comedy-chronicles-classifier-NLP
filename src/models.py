3ecdd13874c70469321f51c197bd59c95e142acb
Ousselhabachi <oussama.elhabachi@gmail.com>
2024-10-28 11:08:44 +0000

    adding the models

--- src/models.py
+++ src/models.py
@@ -1,5 +1,13 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
 
-
-def make_model():
-    return RandomForestClassifier()
+def make_model(model_type="random_forest"):
+    if model_type == "random_forest":
+        return RandomForestClassifier()
+    elif model_type == "naive_bayes":
+        return MultinomialNB()
+    elif model_type == "decision_tree":
+        return DecisionTreeClassifier()
+    else:
+        raise ValueError(f"Unknown model type: {model_type}")
